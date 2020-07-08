00e4619e2fad70fb15c0f08bad821aa96aedbf9f
Your Name <you@example.com>
2020-07-07 21:18:01 -0500

    asd

--- index.php
+++ index.php
@@ -19,36 +19,32 @@ function _pre($data)
     print_r($data);
     echo "</pre>";
 }
+$Charges = new Charges();
+$Snippets = new Snippets();
+$offset = $_REQUEST['offset'];
+$distinctCharges = $Charges->getDistinctCharges($offset);
+try {
+    foreach ($distinctCharges as $k => $charge) {
+        $client = $Charges->getClientsBy($charge["client_id"]); //Datos del cliente
+        $payments = $Charges->getPaymentByChargeId($charge["id"]); //Pagos y abonos de la factura selecionada
+        $previousCharges = $Charges->getDistinctNotPayedCharges($charge["client_id"], $charge["id"]); //Facturas anteriores con saldos
+        $details = $Charges->getChargeDetails($charge["id"]);
+        $services = $Charges->getServicesByStratum($client['stratum_id']);
 
-if (isset($_POST['offset'])) {
-    $Charges = new Charges();
-    $Snippets = new Snippets();
-    $offset = $_REQUEST['offset'];
-    $distinctCharges = $Charges->getDistinctCharges($_POST['offset']);
-    try {
-        foreach ($distinctCharges as $k => $charge) {
-            $client = $Charges->getClientsBy($charge["client_id"]); //Datos del cliente
-            $payments = $Charges->getPaymentByChargeId($charge["id"]); //Pagos y abonos de la factura selecionada
-            $previousCharges = $Charges->getDistinctNotPayedCharges($charge["client_id"], $charge["id"]); //Facturas anteriores con saldos
-            $details = $Charges->getChargeDetails($charge["id"]);
-            $services = $Charges->getServicesByStratum($client['stratum_id']);
+        $html = $Snippets->main($details, $previousCharges, $payments, $charge, $client, $services);
 
-            $html = $Snippets->main($details, $previousCharges, $payments, $charge, $client, $services);
-
-            $mpdf->SetProtection(array('print'));
-            $mpdf->SetTitle("Facturas bulk");
-            $mpdf->showWatermarkText = true;
-            $mpdf->watermark_font = 'DejaVuSansCondensed';
-            $mpdf->watermarkTextAlpha = 0.1;
-            $mpdf->SetDisplayMode('fullpage');
-            $mpdf->WriteHTML($html);
-            $mpdf->AddPage();
-            if ($k == 5) break;
-        }
-    } catch (\Throwable $th) {
-        return ['error' => 'Sucedió un error', 'payload' => $th];
+        $mpdf->SetProtection(array('print'));
+        $mpdf->SetTitle("Facturas bulk");
+        $mpdf->showWatermarkText = true;
+        $mpdf->watermark_font = 'DejaVuSansCondensed';
+        $mpdf->watermarkTextAlpha = 0.1;
+        $mpdf->SetDisplayMode('fullpage');
+        $mpdf->WriteHTML($html);
+        $mpdf->AddPage();
+        if ($k == 5) break;
     }
-    return $mpdf->Output('bulk.pdf', 'I');
-} else {
-    exit(1);
+} catch (\Throwable $th) {
+    return ['error' => 'Sucedió un error', 'payload' => $th];
 }
+
+$mpdf->Output('bulk.pdf', 'I');
